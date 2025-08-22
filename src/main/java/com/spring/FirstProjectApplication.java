37cb4f288b3bca0efbc757746ae1e00062a3edf2
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-23 00:11:23 +0530

    main commit

--- src/main/java/com/spring/FirstProjectApplication.java
+++ src/main/java/com/spring/FirstProjectApplication.java
@@ -3,6 +3,7 @@ package com.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main class
 @SpringBootApplication
 public class FirstProjectApplication {
 
